dff02f4ea2a8af5bcf9f425177153eb05f7cdc35
navin-ghimire <navinghimire143@gmail.com>
2024-08-20 16:33:21 +0545

    okxatw

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,14 +6,24 @@ import mongoose from "mongoose";
 export const getProducts = async (req, res) => {
 
   try {
-    const products = await Product.find({});
-    return res.status(200).json({
-      products,
-    });
+    const product = await Product.find({});
+    return res.status(200).json(product);
+  } catch (err) {
+    return res.status(400).json({ message: `${err}` });
+  }
+}
+
+
+export const getProductById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findById(id);
+    return res.status(200).json(product);
   } catch (er) {
     return res.status(400).json({ error: `${err}` });
   }
 }
+
 
 
 
@@ -28,17 +38,71 @@ export const addProduct = async (req, res) => {
       category,
       description,
       image: req.imagePath,
-      price,
-      stock
+      price: Number(price),
+      stock: Number(stock)
     });
     return res.status(200).json({ message: 'product added succesfully' });
   } catch (err) {
     fs.unlink(`.${req.imagePath}`, (err) => {
       // console.log(err);
     });
-    return res.status(400).json({ error: `${err}` });
+    return res.status(400).json({ message: `${err}` });
   }
 }
+
+
+
+export const updateProduct = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+
+    if (mongoose.isValidObjectId(id)) {
+      const isExist = await Product.findById(id);
+      if (isExist) {
+        const updateObj = {
+          title: req.body.title || isExist.title,
+          brand: req.body.brand || isExist.brand,
+          category: req.body.category || isExist.category,
+          description: req.body.description || isExist.description,
+          stock: Number(req.body.stock) || isExist.stock,
+          price: Number(req.body.price) || isExist.price,
+        };
+        if (req.imagePath) {
+          await isExist.updateOne({
+            ...updateObj,
+            image: req.imagePath
+          });
+          fs.unlink(`.${isExist.image}`, (err) => {
+
+          });
+        } else {
+          await isExist.updateOne(updateObj);
+        }
+
+
+        return res.status(200).json({ message: 'succesfully updated' });
+      }
+
+    }
+
+    if (req.imagePath) fs.unlink(`.${req.imagePath}`, (err) => {
+
+    });
+    return res.status(400).json({ message: 'please provide valid id' });
+
+
+
+  } catch (err) {
+    if (req.imagePath) fs.unlink(`.${req.imagePath}`, (err) => {
+
+    });
+    return res.status(400).json({ message: `${err}` });
+  }
+}
+
+
+
 
 
 
@@ -63,9 +127,7 @@ export const removeProduct = async (req, res) => {
     return res.status(400).json({ message: 'please provide valid id' });
 
   } catch (err) {
-    // fs.unlink(`.${req.imagePath}`, (err) => {
-    //   // console.log(err);
-    // });
+
     return res.status(400).json({ error: `${err}` });
   }
 } 
